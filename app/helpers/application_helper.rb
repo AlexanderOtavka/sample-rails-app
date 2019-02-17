7006018897d30277f7ec7063b4ed1751bd872c7b
Zander Otavka <AlexanderOtavka@users.noreply.github.com>
2019-02-16 19:40:11 -0600

    Add custom page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def title(page_title = "")
+    base_title = "PrettyPics"
+
+    if not page_title.empty?
+      "#{page_title} | #{base_title}"
+    else
+      base_title
+    end
+  end
 end
